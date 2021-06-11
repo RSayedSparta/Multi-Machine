321d3d7816abe43afc6d8a69902d7143b7f9c62a
RahibSayed <50241553+RahibSayed@users.noreply.github.com>
2021-06-11 15:46:56 +0100

    working multi-machine

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Multi-Machine
- 
+# Scripting
+
---
+++ app/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#install node
+sudo apt-get install nodejs -y
+
+#install nodejs needs
+sudo apt-get install python-software-properties
+
+#Uses custom built setup program provided by node
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+
+#re install node
+sudo apt-get install nodejs -y
+sudo npm install
+
+#process manger that will help manage the application
+sudo npm install pm2 -g
